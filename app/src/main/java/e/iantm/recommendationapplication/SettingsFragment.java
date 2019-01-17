6a6ccaf42330755d14f1c572ca79fc2590639367
iantmc87 <iantmcmanus1@outlook.com>
2019-01-17 16:19:13 +0000

    added settings page

--- app/src/main/java/e/iantm/recommendationapplication/SettingsFragment.java
+++ app/src/main/java/e/iantm/recommendationapplication/SettingsFragment.java
@@ -1,17 +1,12 @@
 package e.iantm.recommendationapplication;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends Fragment {
-    @Nullable
+public class SettingsFragment extends PreferenceFragmentCompat {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_settings, null);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.settings);
     }
 }
