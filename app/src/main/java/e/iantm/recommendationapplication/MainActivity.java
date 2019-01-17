00db5dad7a06db8783a060b1f77efde84dfc1c4c
iantmc87 <iantmcmanus1@outlook.com>
2019-01-17 11:11:35 +0000

    Bottom Navigation added

---
+++ app/src/main/java/e/iantm/recommendationapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package e.iantm.recommendationapplication;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+        setTitle("Recommendations");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                setTitle("Recommendations");
+                break;
+
+            case R.id.navigation_reviews:
+                fragment = new ReviewFragment();
+                setTitle("Reviews");
+                break;
+
+            case R.id.navigation_settings:
+                fragment = new SettingsFragment();
+                setTitle("Settings");
+                break;
+
+            case R.id.navigation_help:
+                fragment = new HelpFragment();
+                setTitle("Help");
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
